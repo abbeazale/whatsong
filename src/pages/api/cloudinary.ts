1c330c79fab8c80a824b4b4db486753142be72b2
Abbe Azale <abbeazale98@gmail.com>
2024-09-20 13:10:10 -0700

    more debugging

--- src/pages/api/cloudinary.ts
+++ src/pages/api/cloudinary.ts
@@ -3,7 +3,10 @@ import { v2 as cloudinary } from 'cloudinary';
 if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
   console.error('Cloudinary environment variables are not set');
   throw new Error('Cloudinary configuration is incomplete');
+} else if (process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUDINARY_API_KEY || process.env.CLOUDINARY_API_SECRET) {
+  console.log('Cloudinary environment variables are set');
 }
+
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
